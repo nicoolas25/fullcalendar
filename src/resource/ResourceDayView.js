e3bbf62b1204a9325227a6e0f5df2a706e20e88b
Nicolas ZERMATI <nicoolas25@gmail.com>
2013-11-28 09:49:28 +0100

    Dirty patch to add resourceDay view

---
+++ src/resource/ResourceDayView.js
@@ -0,0 +1,39 @@
+
+fcViews.resourceDay = ResourceDayView;
+
+function ResourceDayView(element, calendar) {
+  var t = this;
+
+  // exports
+  t.render = render;
+
+  // imports
+  ResourceView.call(t, element, calendar, 'resourceDay');
+  var opt = t.opt;
+  var renderAgenda = t.renderAgenda;
+  var skipHiddenDays = t.skipHiddenDays;
+  var formatDates = calendar.formatDates;
+
+  // internals
+  var cellsPerStaff = window.resourceList.length // Uses a global variable for now
+
+  function render(date, delta) {
+
+    if (delta) {
+      addDays(date, delta);
+    }
+
+    var start = cloneDate(date);
+    var end = addDays(cloneDate(start), 1);
+
+    var colCnt = cellsPerStaff;
+
+    t.title = formatDate(date, opt('titleFormat'));
+
+    t.start = t.visStart = start;
+    t.end = t.visEnd = end;
+
+    renderAgenda(colCnt);
+  }
+
+}
